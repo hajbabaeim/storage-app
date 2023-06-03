7ddc39f68402042401cd69444cd3e0c36a3ca457
Mo <mohammadreza@arize.io>
2023-06-03 23:23:09 +0400

    add main & data modules

---
+++ data/data.go
@@ -0,0 +1,63 @@
+package data
+
+import (
+	"encoding/csv"
+	"errors"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"path/filepath"
+	"strconv"
+	"sync"
+	"time"
+)
+
+type Promotion struct {
+	ID             string  `json:"id"`
+	Price          float64 `json:"price"`
+	ExpirationDate string  `json:"expiration_date"`
+}
+
+var PromotionData = make(map[uint64]Promotion)
+var DataLock = &sync.RWMutex{}
+
+func LoadDataEvery30Min() {
+	for {
+		absPath, _ := filepath.Abs("data/promotions.csv")
+		LoadData(absPath)
+		time.Sleep(30 * time.Minute)
+	}
+}
+
+func LoadData(filePath string) {
+	csvFile, err := os.Open(filePath)
+	if err != nil {
+		log.Fatalln(errors.New(fmt.Sprintf("Error during opening the file: %s", err)))
+	}
+	defer csvFile.Close()
+
+	reader := csv.NewReader(csvFile)
+	var count uint64 = 0
+	for {
+		line, err := reader.Read()
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			log.Fatalln(errors.New(fmt.Sprintf("Error during reading the file: %s", err)))
+		}
+
+		price, _ := strconv.ParseFloat(line[1], 64)
+
+		promotion := Promotion{
+			ID:             line[0],
+			Price:          price,
+			ExpirationDate: line[2],
+		}
+
+		DataLock.Lock()
+		PromotionData[count] = promotion
+		count++
+		DataLock.Unlock()
+	}
+}
--- main.go
+++ main.go
@@ -1,5 +1,37 @@
 package main
 
-func main() {
+import (
+	"errors"
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
 
+	"storage/data"
+
+	"github.com/labstack/echo/v4"
+)
+
+func main() {
+	go data.LoadDataEvery30Min()
+	e := echo.New()
+	e.GET("/promotions/:id", getPromotionByID)
+	e.Start(":1321")
+}
+
+func getPromotionByID(c echo.Context) error {
+	id := c.Param("id")
+	uintId, err := strconv.ParseUint(id, 10, 64)
+	if err != nil {
+		log.Fatalln(errors.New(fmt.Sprintf("Input id is not valid: %s", err)))
+	}
+	data.DataLock.RLock()
+	promotion, ok := data.PromotionData[uintId]
+	data.DataLock.RUnlock()
+
+	if !ok {
+		return c.JSON(http.StatusNotFound, map[string]string{"error": "Not found"})
+	}
+
+	return c.JSON(http.StatusOK, promotion)
 }
