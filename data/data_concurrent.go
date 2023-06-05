4e51b91e3d79ae7e9daa0a76fb0eddf1207eca81
Mo <mohammadreza@arize.io>
2023-06-05 19:17:37 +0400

    add promotion with line-number struct

--- data/data_concurrent.go
+++ data/data_concurrent.go
@@ -4,82 +4,114 @@ import (
 	"encoding/csv"
 	"io"
 	"os"
+	"path/filepath"
 	"strconv"
 	"time"
 )
 
 const (
-	BatchSize     = 1000 // Process 1000 lines at a time
-	MaxGoroutines = 100  // Maximum number of concurrent goroutines
+	BatchSize     uint64 = 1000 // Process 1000 lines at a time
+	MaxGoroutines uint64 = 100  // Maximum number of concurrent goroutines
 )
 
 var sem = make(chan struct{}, MaxGoroutines) // Semaphore to limit concurrent goroutines
 
+type PromotionWithLineNumber struct {
+	Promotion
+	LineNumber uint64
+}
+
 func LoadDataConcurrentEvery30Min() {
 	for {
-		LoadDataConcurrent("promotions.csv")
+		absPath, _ := filepath.Abs("data/promotions.csv")
+		LoadDataConcurrent(absPath)
 		time.Sleep(30 * time.Minute)
 	}
 }
 
 func LoadDataConcurrent(filePath string) {
-	csvFile, err := os.Open(filePath)
-	if err != nil {
-		panic(err)
-	}
-	defer csvFile.Close()
+	lines := readCsv(filePath)
+	promotions := processBatches(lines)
+	storeBatches(promotions)
+}
 
-	reader := csv.NewReader(csvFile)
+func readCsv(filePath string) <-chan []string {
+	out := make(chan []string)
 
-	batch := make([]Promotion, 0, BatchSize)
-	var lineNumber uint64 = 0
-
-	for {
-		line, err := reader.Read()
-		if err == io.EOF {
-			go func(b []Promotion, startLineNumber uint64) {
-				sem <- struct{}{}
-				processBatchConcurrent(b, startLineNumber) // Pass the start line number for this batch
-				<-sem
-			}(batch, lineNumber-uint64(len(batch)))
-			break
-		} else if err != nil {
+	go func() {
+		csvFile, err := os.Open(filePath)
+		if err != nil {
 			panic(err)
 		}
+		defer csvFile.Close()
 
-		price, _ := strconv.ParseFloat(line[1], 64)
+		reader := csv.NewReader(csvFile)
 
-		promotion := Promotion{
-			ID:             line[0], // You may want to remove this line if you're not using the ID field anymore
-			Price:          price,
-			ExpirationDate: line[2],
+		for {
+			line, err := reader.Read()
+			if err == io.EOF {
+				close(out)
+				break
+			} else if err != nil {
+				panic(err)
+			}
+
+			out <- line
+		}
+	}()
+
+	return out
+}
+
+func processBatches(lines <-chan []string) <-chan PromotionWithLineNumber {
+	out := make(chan PromotionWithLineNumber)
+
+	go func() {
+		var lineNumber uint64 = 1 // Start from 1, because CSV header is not a record.
+		batch := make([]Promotion, 0, BatchSize)
+
+		for line := range lines {
+			price, _ := strconv.ParseFloat(line[1], 64)
+
+			promotion := Promotion{
+				ID:             line[0],
+				Price:          price,
+				ExpirationDate: line[2],
+			}
+
+			batch = append(batch, promotion)
+			lineNumber++
+
+			if uint64(len(batch)) >= BatchSize {
+				go processBatchConcurrent(batch, lineNumber-BatchSize, out)
+				batch = make([]Promotion, 0, BatchSize)
+			}
 		}
 
-		batch = append(batch, promotion)
-
-		if uint64(len(batch)) >= BatchSize {
-			go func(b []Promotion, startLineNumber uint64) {
-				sem <- struct{}{}
-				processBatchConcurrent(b, startLineNumber) // Pass the start line number for this batch
-				<-sem
-			}(batch, lineNumber-uint64(len(batch))+1)
-			batch = make([]Promotion, 0, BatchSize) // Reset batch
+		if len(batch) > 0 {
+			go processBatchConcurrent(batch, lineNumber-uint64(len(batch)), out)
 		}
+	}()
 
-		lineNumber++
-	}
+	return out
+}
 
-	// Wait for all goroutines to finish
-	for i := 0; i < cap(sem); i++ {
-		sem <- struct{}{}
+func processBatchConcurrent(batch []Promotion, startLineNumber uint64, out chan<- PromotionWithLineNumber) {
+	sem <- struct{}{}
+	defer func() { <-sem }()
+	for i, promotion := range batch {
+		out <- PromotionWithLineNumber{
+			Promotion:  promotion,
+			LineNumber: startLineNumber + uint64(i),
+		}
 	}
 }
 
-func processBatchConcurrent(batch []Promotion, startLineNumber uint64) {
+func storeBatches(promotions <-chan PromotionWithLineNumber) {
 	DataLock.Lock()
-	for i, promotion := range batch {
-		PromotionData[startLineNumber+uint64(i)] = promotion
+	defer DataLock.Unlock()
+
+	for p := range promotions {
+		PromotionData[p.LineNumber] = p.Promotion
 	}
-	DataLock.Unlock()
-	println("Processed a batch of size:", len(batch))
 }
